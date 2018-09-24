2f8eb78996ebbf269588fcd81f83735c240a119b
Quijas <joseq4101@gmail.com>
2018-09-23 22:25:39 -0700

    Updated notes-app and todo-app

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -1,15 +1,51 @@
-//DOM - Document Object Model
+const notes = [{
+    title: 'My next Trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on', 
+    body: 'Excercise. Eating a bit better '
+}, {
+    title: 'Office modification', 
+    body: 'Get a new seat'
+}, {
+    title: 'Play League', 
+    body: 'Practice new champs'
+}, {
+    title: 'Work schedule', 
+    body: 'Create schedule for employees'
+},]
 
-//Query and remove
-/* const p = document.querySelector('p')
-p.remove() */
+const filters = {
+    searchText: ''
+}
 
-//query all and remove
-const ps = document.querySelectorAll('p') // finds all p tags
+const renderNotes = function(notes,filters){
+    const filteredNotes = notes.filter(function(note){ 
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
-ps.forEach(function(p){
+    document.querySelector('#notes').innerHTML = '' //wipes all previous rendered notes
 
-    p.textContent = '******'
-    //console.log(p.textContent)
-    // p.remove()
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters) // initial rendering
+
+document.querySelector('#create-notes').addEventListener('click',function(e){
+    e.target.textContent = 'This button was clicked'
+})
+
+document.querySelector('#search-text').addEventListener('input',function(e){
+    filters.searchText = e.target.value
+    renderNotes(notes, filters) //render latest notes every time input changes
+})
+
+document.querySelector('#name-form').addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log(e.target.elements.firstName.value)
+    e.target.elements.firstName.value = ''
 })
