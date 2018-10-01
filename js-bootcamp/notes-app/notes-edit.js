229635bce6cc60339e18874a3f60732fbb64ad55
Quijas <joseq4101@gmail.com>
2018-09-30 20:14:31 -0700

    notes-app auto change with local storage

--- js-bootcamp/notes-app/notes-edit.js
+++ js-bootcamp/notes-app/notes-edit.js
@@ -3,9 +3,9 @@ const bodyElement = document.querySelector('#note-body')
 const removeButton = document.querySelector('#remove-note')
 const saveButton = document.querySelector('#save-note')
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
-const note = notes.find(function(note){
+let note = notes.find(function(note){
     return note.id === noteId
 })
 
@@ -28,4 +28,20 @@ saveButton.addEventListener('click', function(e){
     note.body = bodyElement.value
     savedNotes(notes)
     location.assign('./index.html')
+})
+
+window.addEventListener('storage', function(e){
+    if(e.key === 'notes'){
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function(note){
+            return note.id === noteId
+        })
+        
+        if(note === undefined){
+            location.assign('./index.html')
+        }
+        
+        titleElement.value = note.title
+        bodyElement.value= note.body
+    }
 })
