acc8508264ebda8fa5fb72b24a93fbb29735dda8
Quijas <joseq4101@gmail.com>
2018-10-09 22:46:58 -0700

    updates

---
+++ js-bootcamp/advanced/type-coercion.js
@@ -0,0 +1,12 @@
+//Type Coercion -  string, number, boolean
+
+console.log('5' + 5)
+console.log('5' - 5)
+console.log(5 === '5') //false
+console.log(5 == '5') //true
+
+const value = false + 12
+const type = typeof value
+
+console.log(type)
+console.log(value)
--- js-bootcamp/functions/grade-calc.js
+++ js-bootcamp/functions/grade-calc.js
@@ -3,9 +3,14 @@
 // A 90-100, B 80-89, C 70-79, D 60-69, F 0-59 
 
 const getGrade = function (Score, possibleScore) {
+
+    if (typeof Score !== 'number' || typeof possibleScore !== 'number' ){
+        throw Error('Please enter numbers')
+    }
+    
     const grade = (100/possibleScore) * Score  
     let letterGrade = ''
-    
+
     if(grade  >= 90){
         letterGrade = 'A'
     }else if (grade  >= 80){
@@ -18,10 +23,12 @@ const getGrade = function (Score, possibleScore) {
         letterGrade = 'F'
     }
     return `${Score}/${possibleScore} -> You got a ${letterGrade} (${grade}%)!`
-
 }
 
-const result = getGrade(120,155)
+try{
+    const result = getGrade(100,true)
 
-console.log(result)
-
+    console.log(result)
+}catch(e){
+    console.log(e.message)
+}
