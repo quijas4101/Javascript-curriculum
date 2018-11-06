64d72bffa99250e0fa1b5dd79163a75021f22aa4
Quijas <joseq4101@gmail.com>
2018-11-05 19:34:44 -0800

    changes

--- js-bootcamp/hangman/hangman.js
+++ js-bootcamp/hangman/hangman.js
@@ -1,13 +1,14 @@
 
-const Hangman = function(word , remainingGuesses){
+class Hangman {
+    constructor(word , remainingGuesses){
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
     this.guessedLetters = []
     this.status = 'Playing'
-}
+    }
 
-Hangman.prototype.calculateStatus = function () {
-const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
+    calculateStatus(){
+    const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
 
     if (this.remainingGuesses === 0){
         this.status='Failed'
@@ -15,20 +16,20 @@ const finished = this.word.every((letter) => this.guessedLetters.includes(letter
         this.status = 'Finished'
     }else{
         this.status ='Playing'
+        }
     }
-}
 
-Hangman.prototype.getStatusMessage = function(){
-    if(this.status === 'Playing'){
+    getStatusMessage(){
+     if(this.status === 'Playing'){
         return `Remaining Guesses: ${this.remainingGuesses}`
     } else if (this.status == 'Failed'){
         return `Nice try! the word was "${this.word.join('')}"`
     }else{
         return "Great Work! You guessed the word"
+    }       
     }
-}
 
-Hangman.prototype.getPuzzle = function(){
+    getPuzzle(){
     let puzzle = ''
 
     this.word.forEach((letter) => {
@@ -39,25 +40,30 @@ Hangman.prototype.getPuzzle = function(){
             puzzle += '*' 
         }
     })
-    return puzzle
-}
+    return puzzle       
+    }
 
-Hangman.prototype.makeGuess = function (guess){
+    makeGuess(guess){
     guess =  guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)
     const isBadGuess = !this.word.includes(guess)
-
+    
     if(this.status !== 'Playing'){
         return
     }
-
+    
     if(isUnique){
         this.guessedLetters.push(guess)
     }
-
+    
     if(isUnique && isBadGuess){
         this.remainingGuesses--
     }
-
+    
     this.calculateStatus()
+    }
 }
+
+
+
+
--- js-bootcamp/oop/person.js
+++ js-bootcamp/oop/person.js
@@ -1,33 +1,59 @@
 // Prototypal Inheritance
 
-const Person = function(firstName, lastName, age, likes = []){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
+class Person {
+    constructor(firstName, lastName, age, likes = []){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
 
-Person.prototype.getBio = function (){
-    let bio = `${this.firstName} is ${this.age}.`
+    getBio(){
+        let bio = `${this.firstName} is ${this.age}.`
     
     this.likes.forEach((like) => {
         bio += ` ${this.firstName} likes ${like}.`
     })
     return bio
-}
+    }
 
-Person.prototype.setName  =  function (fullName){
+    setName(fullName){
     const names = fullName.split(' ')
     this.firstName = names[0]
     this.lastName = names[1]
+    }
 }
-                 
-const me = new Person('Jose', 'Quijas', 28, ['Teaching', ' Running'])
 
-me.setName('Jean-luc Quijas')
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes = []){
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio(){
+        return `${this.firstName} ${this.lastName} is a ${this.position}`
+    }
+    getYearsLeft(){
+        return 65 - this.age
+    }
+}
 
-console.log(me.getBio())
+class Student extends Person {
+    constructor(firstName, lastName, age, grade, likes = []){
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    getBio(){
+        const status = this.grade >= 70 ? 'passing' : 'failing'
+        return `${this.firstName} is ${status} the class! with ${this.grade}%`
+    }
 
-const person2 = new Person('Clancy', 'Turner', 51)
+    updateGrade(grade){
+        this.grade += grade
+    }
+}
 
-console.log(person2.getBio())
+const kid = new Student('Jose', 'Quijas', 15, 100, ['pizza', 'cars'])
+console.log(kid.getBio())
+
+kid.updateGrade(-31)
+console.log(kid.getBio())
